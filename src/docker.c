e032f987cdb040cb4de8e42b812e8c770b8d697b
Guillaume Fieni <gfieni@users.noreply.github.com>
2018-09-20 15:37:46 +0200

    feat: Add helper function to get Docker container name from its full id

---
+++ src/docker.c
@@ -0,0 +1,41 @@
+#include <bson.h>
+#include <string.h>
+
+#include "docker.h"
+
+char *
+docker_get_container_name_from_id(const char *full_id)
+{
+    char config_path[CONFIG_PATH_BUFFER_SIZE];
+    int r;
+    bson_json_reader_t *reader = NULL;
+    bson_t doc = BSON_INITIALIZER;
+    bson_error_t error;
+    bson_iter_t iter_doc;
+    bson_iter_t iter_name;
+    char *container_name = NULL;
+
+    r = snprintf(config_path, CONFIG_PATH_BUFFER_SIZE, "/var/lib/docker/containers/%s/config.v2.json", full_id);
+    if (r <= 0 && r > CONFIG_PATH_BUFFER_SIZE)
+        goto end;
+
+    reader = bson_json_reader_new_from_file(config_path, &error);
+    if (!reader)
+        goto end;
+
+    r = bson_json_reader_read(reader, &doc, &error);
+    if (r < 0)
+        goto end;
+
+    if (!bson_iter_init(&iter_doc, &doc) || !bson_iter_find_descendant(&iter_doc, "Name", &iter_name) || !BSON_ITER_HOLDS_UTF8(&iter_name))
+        goto end;
+
+    /* copy name without the leading slash */
+    container_name = strdup(bson_iter_utf8(&iter_name, 0) + 1); 
+
+end:
+    bson_json_reader_destroy(reader);
+    bson_destroy(&doc);
+    return container_name;
+}
+
---
+++ src/docker.h
@@ -0,0 +1,18 @@
+#ifndef DOCKER_H
+#define DOCKER_H
+
+/*
+ * CONFIG_PATH_BUFFER_SIZE stores the max length of the path to the container json config file.
+ * /var/lib/docker/containers/756535dc6e9ab9b560f84c85063f55952273a23192641fc2756aa9721d9d1000/config.v2.json = 106
+ */
+#define CONFIG_PATH_BUFFER_SIZE 128
+
+/*
+ * docker_container_get_name_from_id returns the name of the container having the given *full* id.
+ * The returned container name *have* to be deallocated after use.
+ * In case of failure, the function returns NULL.
+ */
+char *docker_get_container_name_from_id(const char *full_id);
+
+#endif /* DOCKER_H */
+
